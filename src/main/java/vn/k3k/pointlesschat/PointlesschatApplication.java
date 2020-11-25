ed296b667f863cc296d415e430a92698c51fe3df
Kha Tran <khabaka321998@gmail.com>
2020-11-25 10:42:34 +0700

    init

---
+++ src/main/java/vn/k3k/pointlesschat/PointlesschatApplication.java
@@ -0,0 +1,13 @@
+package vn.k3k.pointlesschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointlesschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointlesschatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/k3k/pointlesschat/PointlesschatApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.k3k.pointlesschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointlesschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
